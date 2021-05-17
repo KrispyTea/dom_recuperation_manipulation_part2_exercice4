109f2937a976fee0c04d0d88cb836b360692fad5
KrispyTea <80962605+KrispyTea@users.noreply.github.com>
2021-05-17 13:40:59 +0200

    over

---
+++ main.js
@@ -0,0 +1,32 @@
+let competences = {
+    HTML: '71%',
+    CSS: '95%',
+    JS : '50%',
+    React: '18%',
+    Laravel: '100%'
+}
+
+let elements = document.querySelectorAll("#liste-competences > h2")
+let i = 0;
+console.log(elements);
+for (const el in competences) {
+    let titre = elements[i]
+    titre.innerText = `${competences[el]} `
+    let competLength = competences[el].length
+    let verif = competences[el].substr(0, competLength -1)
+
+
+    if ( verif > 50 && verif < 100 && verif != 50 ){
+        titre.style.backgroundColor = "green"
+
+        titre.style.color = "white"
+    }
+        else if (verif ==100) {
+            titre.style.backgroundColor = "gold"
+        }
+        else if (verif <50) {
+            titre.style.backgroundColor = "red"
+        }
+        titre.style.width =`${verif}%`
+        i++
+    }
